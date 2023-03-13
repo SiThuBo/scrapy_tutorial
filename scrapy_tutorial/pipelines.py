d5de7609a0ccf0916602b1b8dcadd76610199e12
SiThuBo <sithu.bo@seattleconsultingmyanmar.com>
2023-03-13 12:33:22 +0630

    [add] create database adn table

--- scrapy_tutorial/pipelines.py
+++ scrapy_tutorial/pipelines.py
@@ -11,8 +11,9 @@ import mysql.connector
 
 class ScrapyTutorialPipeline:
 
-    def __init__(self, mysql_host, mysql_user, mysql_password, mysql_database):
+    def __init__(self, mysql_host, mysql_port, mysql_user, mysql_password, mysql_database):
         self.mysql_host = mysql_host
+        self.mysql_port = mysql_port
         self.mysql_user = mysql_user
         self.mysql_password = mysql_password
         self.mysql_database = mysql_database
@@ -21,6 +22,7 @@ class ScrapyTutorialPipeline:
     def from_crawler(cls, crawler):
         return cls(
             mysql_host=crawler.settings.get('MYSQL_HOST'),
+            mysql_port=crawler.settings.get('MYSQL_PORT'),
             mysql_user=crawler.settings.get('MYSQL_USER'),
             mysql_password=crawler.settings.get('MYSQL_PASSWORD'),
             mysql_database=crawler.settings.get('MYSQL_DATABASE')
@@ -28,11 +30,31 @@ class ScrapyTutorialPipeline:
 
     def open_spider(self, spider):
         self.conn = mysql.connector.connect(
-            host=self.mysql_host,
-            user=self.mysql_user,
-            password=self.mysql_password,
-            database=self.mysql_database
-        )
+			host=self.mysql_host,
+			port=self.mysql_port,
+			user=self.mysql_user,
+			password=self.mysql_password,
+		)
+
+        self.curr = self.conn.cursor()
+
+        # Create the database if it doesn't exist
+        self.curr.execute(f"CREATE DATABASE IF NOT EXISTS {self.mysql_database}")
+        self.conn.commit()
+
+        # Select the database
+        self.curr.execute(f"USE {self.mysql_database}")
+
+        # Create the table if it doesn't exist
+        self.curr.execute("""
+            CREATE TABLE IF NOT EXISTS portfolio (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                title VARCHAR(255),
+                details_link VARCHAR(255),
+                image VARCHAR(255)
+            )
+        """)
+        self.conn.commit()
 
     def close_spider(self, spider):
         self.conn.close()
